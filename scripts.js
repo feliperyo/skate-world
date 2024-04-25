3c74c3f87f3f653f44622c50a58bd2fb6136e77c
feliperyo <ryofeliperyo@gmail.com>
2024-04-25 11:16:11 -0300

    adicionando animações

---
+++ scripts.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.title-1', { duration: 1500 })
+
+sr.reveal('.title-2', { duration: 1500 })
+
+sr.reveal('.title-3', { duration: 1500 })
+
+sr.reveal('.title-4', { duration: 1500 })
+
+sr.reveal('.title-5', { duration: 1500 })
